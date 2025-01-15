086986d0dd687ed3831c8f5af7e849ec3a228936
Carly <42504974+Carly7766@users.noreply.github.com>
2025-01-15 11:51:18 +0900

    Add mockup

---
+++ Assets/Escaplanet/Mockup/Scripts/Ingame/Attractable/AttractableView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Escaplanet.Mockup.Ingame.Attractable
+{
+    public class AttractableView : MonoBehaviour, IAttractable
+    {
+        public Vector2 Position => transform.position;
+
+        private Rigidbody2D _rigidbody2D;
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        public void Attract(Vector2 acceleration)
+        {
+            _rigidbody2D.AddForce(acceleration, ForceMode2D.Force);
+        }
+    }
+}
